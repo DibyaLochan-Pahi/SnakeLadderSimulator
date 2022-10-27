634f8c0c384a71752544aa04d73e9a77684d2e39
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-27 21:25:39 +0530

    Added UC1 Game Played With Single Player

---
+++ LudoUC/UC1GamePlaySinglePlayer/UC1GamePlaySinglePlayer/UC1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC1GamePlaySinglePlayer
+{
+    internal class UC1
+    {
+        public UC1()
+        {
+            Console.WriteLine("Welcome To Snake and Ladder Simulator");
+            Console.WriteLine("--------------------------------------");
+        }
+        static void Main(string[] args)
+        {
+            UC1 game = new UC1();
+            int startingPoint = 0;
+            Console.WriteLine("Player Starting Point is : "+ startingPoint);
+        }
+    }
+}
