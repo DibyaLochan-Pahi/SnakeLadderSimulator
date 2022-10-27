5eca46b56bc9e4d9b8aa669ccb76e89dc3b944ef
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-27 22:06:04 +0530

    Added UC2 Rolls the Die to get a number

---
+++ UC2RollDiceAndGetNumber/UC2RollDiceAndGetNumber/UC2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UC2RollDiceAndGetNumber
+{
+    internal class UC2
+    {
+        //Constructor for Print Heading
+        public UC2()
+        {
+            Console.WriteLine("Welcome To Snake and Ladder Simulator");
+            Console.WriteLine("--------------------------------------");
+        }
+        static void Main(string[] args)
+        {
+            UC2 game = new UC2();       //calling the Constructor
+            int startingPoint = 0;
+            Random random = new Random();
+            Console.WriteLine("Player Starting Point is : " + startingPoint);
+            int newPoint=GetNumber(random);                 // calling the method
+            startingPoint = startingPoint + newPoint;       //shifted to new point
+            Console.WriteLine("Shift New Point : "+ startingPoint);
+            
+        }
+        //Method For Get Random Number 
+        public static int GetNumber(Random random)
+        {
+            int newPoint = random.Next(0, 7);
+            Console.WriteLine(" New Point is : "+ newPoint);
+            return newPoint;
+        }
+    }
+}
